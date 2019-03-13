31b8126afd7856ff14e5efda1dc97e75cbba3c9e
superchel <for.oleg.mozolev@gmail.com>
2019-03-13 19:47:32 +0200

    fixed display of the filter panel

--- app/services/advanced_filters.rb
+++ app/services/advanced_filters.rb
@@ -11,28 +11,29 @@ class AdvancedFilters
     result_hash   = { properties: nil, product_properties: nil,
                       products:   [],   price_range:      nil }
 
-    products    = filters_merger(params_clone, @products).distinct
-    result_hash[:products] = products
-    return result_hash if products.blank?
+    return result_hash if @products.blank?
 
-    min_price              = products.map(&:price).min.to_i
-    max_price              = products.map(&:price).max.round.to_i
+    min_price              = @products.map(&:price).min.to_i
+    max_price              = @products.map(&:price).max.round.to_i
     price_range            = {min_range: params_clone[:min_price_range].present? ? params_clone[:min_price_range].to_i : min_price,
                               max_range: params_clone[:max_price_range].present? ? params_clone[:max_price_range].to_i : max_price,
                               collection_min_price: min_price,
                               collection_max_price: max_price}
     result_hash[:price_range] = price_range
 
-    properties               = products.includes(:properties).map(&:properties).flatten.uniq
+    properties               = @products.includes(:properties).map(&:properties).flatten.uniq
     result_hash[:properties] = allowed_properties(properties) unless properties.blank?
 
-    product_properties               = products.map(&:product_properties).flatten.uniq.map(&:value).uniq
+    product_properties               = @products.map(&:product_properties).flatten.uniq.map(&:value).uniq
     result_hash[:product_properties] = product_properties unless product_properties.blank?
 
     if @params[:controller] == 'spree/taxons' && @params[:action] == 'show'
-      option_types               = products.includes(:option_types).map(&:option_types).flatten.uniq
+      option_types               = @products.includes(:option_types).map(&:option_types).flatten.uniq
       result_hash[:option_types] = option_types unless option_types.blank?
     end
+
+    products    = filters_merger(params_clone, @products).distinct
+    result_hash[:products] = products
 
     result_hash
   end
